1f4e154094ef54abb7e494c0f576ff6b0c8fa5cf
nirayal <nir.ayal@gmail.com>
2023-01-17 18:21:06 +0200

    define Ques

--- questions_define.php
+++ questions_define.php
@@ -14,7 +14,18 @@
     define('QUES_137','what is the avarge time that you spends on the way to the collage?');
     define('QUES_138','what is the distance to the collage from your home?');
 
-   
-    
+    define('QUES_21','how satisfied are you with the availability and reliability of public transportation in your area?');
+    define('QUES_22','have you noticed any improvements or declines in public transportation in your area in recent years?');
+    define('QUES_23','do you think the collage should invest in public transportation?');
+    define('QUES_231','name one way that the collage should invest to inprove public transportation?'); //if yes - JS
+    define('QUES_24','How do you think the cost of public transportation compares to the cost of owning and maintaining a personal vehicle?');
+    define('QUES_25','How safe do you feel using public transportation?');
+
+    define('QUES_31','would you use a share light transportation such as e-scooter or e-bike?');
+    define('QUES_32','have you noticed any improvements or declines in public light transportation in your area in recent years?');
+    define('QUES_331','name one way that the goverment should invest to inprove public light transportation?'); //if yes - JS
+    define('QUES_33','How do you think the cost of public transportation compares to the cost of owning and maintaining a personal light transportation vehicle?');
+    define('QUES_34','How safe do you feel using shared self-light transportation?');
+
 
 ?>
