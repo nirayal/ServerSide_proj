56575a41d0e2d09782c7888f899e5c0ca5644e60
nirayal <nir.ayal@gmail.com>
2023-02-12 13:33:08 +0200

    asd

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$urlContents = "https://data.gov.il/api/3/action/datastore_search?resource_id=1b14e41c-85b3-4c21-bdce-9fe48185ffca&limit=5";
-$data = file_get_contents($urlContents);
-$cities = json_decode($urlContents, true);
-print_r($cities);
-
-
-?>
